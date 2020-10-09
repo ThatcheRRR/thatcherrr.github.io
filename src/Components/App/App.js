52a9836eb37c8040d7eacd610d9a905ff655af12
ThatcheRRR <pavelosipov777@mail.ru>
2020-10-09 17:12:02 +0300

    feat: add wowjs and react-router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import WOW from 'wowjs';
+import './app.scss';
 
 const App = () => {
+  useEffect(() => {
+    new WOW.WOW().init();
+	}, []);
+
   return (
-    <h1>h1</h1>
+    <h1 className="wow slideInLeft" data-wow-duration="1s" data-wow-delay="0.5s">An animated element</h1>
   );
 }
 
