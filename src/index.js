c84495550344805ae9b6bf1f3eba2388594e3767
ThatcheRRR <pavelosipov777@mail.ru>
2020-10-09 13:38:58 +0300

    init: add base structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import App from './Components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
