618dd7e5ec193bef9553e66625f64ad14c18a9ff
ThatcheRRR <pavelosipov777@mail.ru>
2020-10-11 14:45:29 +0300

    feat: change header icon & add bg to about section

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 import Navigation from '../Navigation';
 import './header.scss';
 
@@ -6,7 +9,9 @@ const Header = ({className}) => {
   return (
     <header className = {`header ${className}`}>
       <div className = 'header__container'>
-        <h1 className = 'header__author'>Pavel Osipau</h1>
+        <Link to = '/'>
+          <FontAwesomeIcon icon = {faCode} size = '2x' />
+        </Link>
         <Navigation />
       </div>
     </header>
