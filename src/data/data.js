8c1d60c9896e747c05fd2e79bbc6a013669d35cb
ThatcheRRR <pavelosipov777@mail.ru>
2020-10-11 16:04:20 +0300

    feat: add images to all tasks in portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -25,7 +25,7 @@ export const contacts = [
 
 export const portfolio = [
     {
-        title: 'RSS Schedule',
+        title: 'RSS Schedule (team task)',
         stack: [
             'React',
             'Redux',
@@ -33,12 +33,12 @@ export const portfolio = [
             'Yandex Geocoder API',
             'Mapbox API'
         ],
-        image: '',
+        image: 'https://hhcdn.ru/photo/599687592.png?t=1602505380&h=Me6_KraikILBIN206kXpug',
         deploy: 'https://schedule-team20.netlify.app/',
         repo: 'https://github.com/React-team-20/schedule'
     },
     {
-        title: 'Songbird',
+        title: 'GamesOSTs quiz',
         stack: [
             'React',
             'Redux',
@@ -46,7 +46,7 @@ export const portfolio = [
             'Grid',
             'SCSS'
         ],
-        image: '',
+        image: 'https://hhcdn.ru/photo/596322447.png?t=1602505380&h=N9EgSLpW5aVLUh5-H0iyFg',
         deploy: 'https://thatcherrr.github.io/songbird/',
         repo: 'https://github.com/ThatcheRRR/songbird'
     },
@@ -60,7 +60,7 @@ export const portfolio = [
             'Geocode API',
             'Weather API'
         ],
-        image: '',
+        image: 'https://hhcdn.ru/photo/596322595.png?t=1602505380&h=nT5oYIw_NK8kPxI_cMAitg',
         deploy: 'https://thatcherrr.github.io/fancy-weather/',
         repo: 'https://github.com/ThatcheRRR/fancy-weather'
     },
@@ -71,7 +71,7 @@ export const portfolio = [
             'Canvas',
             'SCSS'
         ],
-        image: '',
+        image: 'https://hhcdn.ru/photo/596322591.png?t=1602505380&h=TJnxJP4mCmHVfxEYqqLPzA',
         deploy: 'https://thatcherrr.github.io/simple-piskel-clone/',
         repo: 'https://github.com/ThatcheRRR/simple-piskel-clone'
     },
@@ -82,16 +82,16 @@ export const portfolio = [
             'Canvas',
             'Image API'
         ],
-        image: '',
+        image: 'https://hhcdn.ru/photo/596323567.png?t=1602505380&h=REqpLB0OWFOdHcrm6f8gfg',
         deploy: 'https://thatcherrr.github.io/codejam-image-api/',
         repo: 'https://github.com/ThatcheRRR/codejam-image-api/tree/codejam-image-api'
     },
     {
         title: 'TODO App',
         stack: [
-            'React'
+            'React',
         ],
-        image: '',
+        image: 'https://hhcdn.ru/photo/600899803.png?t=1602507707&h=6QmOoW6tFHQ0O3qCYGIf5g',
         deploy: 'https://thatcherrr.github.io/todo-task/',
         repo: 'https://github.com/ThatcheRRR/todo-task'
     },
@@ -100,8 +100,20 @@ export const portfolio = [
         stack: [
             'JavaScript',
         ],
-        image: '',
+        image: 'https://hhcdn.ru/photo/596323283.png?t=1602505380&h=zZTUvlB3ka096_94YDvw6w',
         deploy: 'https://thatcherrr.github.io/codejam-virtual-keyboard/',
         repo: 'https://github.com/ThatcheRRR/codejam-virtual-keyboard/tree/codejam-virtual-keyboard'
+    },
+    {
+        title: 'Singolo',
+        stack: [
+            'HTML5',
+            'CSS3',
+            'Flex',
+            'Grid'
+        ],
+        image: 'https://hhcdn.ru/photo/596323103.png?t=1602507707&h=gRmU5lvou-NCHJzF9uWUxg',
+        deploy: 'https://thatcherrr.github.io/singolo/',
+        repo: 'https://github.com/ThatcheRRR/singolo'
     },
 ];
