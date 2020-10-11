9ad6a06f3b2098450bb23cdc4a2a62009aada97a
ThatcheRRR <pavelosipov777@mail.ru>
2020-10-11 18:19:31 +0300

    feat: add blur to navigtaion

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,8 +5,6 @@ const About = () => {
   return(
     <section className = 'main__about about'>
       <div className = 'about__container'>
-        dasdsagfdgdfgdf
-        gdfgfd
         <div>dasdsada</div>
         <div>dasdsada</div>
         <div>dasdsada</div>
